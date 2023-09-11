499ff8bc9eebfebc8cdac2592937e107b044bcd1
Raze100 <rawatrohit084@gmail.com>
2023-09-11 12:12:17 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const cron = require('node-cron')
 
-const { sendBasicEmail } = require('./services/email-service')
+const { sendBasicEmail } = require('./services/email-service');
+const req = require('express/lib/request');
 
 const setUpAndStartServer = () => {
 
@@ -14,12 +16,15 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.listen(PORT,()=>{
     console.log(`Server Started on PORT: ${PORT}`);
 
-    sendBasicEmail(
-        'support@admin.com',
-        'rohitmails084@gmail.com',
-        'This is a testing email',
-        'Hey, how are you, I hope you like the support'
-    )
+    // sendBasicEmail(
+    //     'support@admin.com',
+    //     'rohitmails084@gmail.com',
+    //     'This is a testing email',
+    //     'Hey, how are you, I hope you like the support'
+    // )
+    // cron.schedule('*/2 * * * *',() => {
+    //     console.log('running a task every two minutes');
+    // })
 })
 }
 
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,14 +1,18 @@
 const sender = require('../config/emailConfig');  
 
 const sendBasicEmail = async (mailFrom, MailTo, mailSubject, mailBody) => {
-    const response = await sender.sendMail({
-        from: mailFrom,
-        to: MailTo,
-        subject: mailSubject,
-        text: mailBody
-    });   
-
-    console.log(response);
+    try {
+        const response = await sender.sendMail({
+            from: mailFrom,
+            to: MailTo,
+            subject: mailSubject,
+            text: mailBody
+        });  
+        console.log(response);
+    } 
+    catch (error) {
+        console.log(error);
+    } 
 }
 
 module.exports = {
